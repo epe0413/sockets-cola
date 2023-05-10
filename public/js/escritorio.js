e285e6b0f66c0a59c08a6c6ba5538363c526c294
Everth Paredes <epe0413@gmail.com>
2023-05-10 17:46:32 -0600

    Preparar pantalla de escritorio

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -1,3 +1,39 @@
+// Referencias HTML
+const lblEscritorio = document.querySelector('h1');
+const btnAtender = document.querySelector( 'button' )
+
+const searchParams = new URLSearchParams( window.location.search );
+
+if( !searchParams.has('escritorio')) {
+    window.location = 'index.html'
+    throw new Error('El escritorio es obligatorio')
+}
+
+const escritorio = searchParams.get('escritorio');
+lblEscritorio.innerText = escritorio
+
+console.log(escritorio)
+
+const socket = io();
+
+socket.on('connect', () => {
+    btnAtender.disabled = false
+
+});
+
+socket.on('disconnect', () => {
+    btnAtender.disabled = true
+});
+
+socket.on('ultimo-ticket', (ultimo) => {
+    // lblNuevoTicket.innerText = 'Ticket ' + ultimo ;
+})
 
 
-console.log('Escritorio HTML');
+btnAtender.addEventListener( 'click', () => {
+    
+    // socket.emit( 'siguiente-ticket', null, (ticket) => {
+    //     lblNuevoTicket.innerText = ticket;
+    // });
+
+});
