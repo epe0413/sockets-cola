35b0fa52b8e2f6f4a53451c2bb8ce87c2dde65ef
Everth Paredes <epe0413@gmail.com>
2023-05-10 13:36:15 -0600

    Socket: Siguiente Ticket

--- sockets/controller.js
+++ sockets/controller.js
@@ -3,13 +3,15 @@ const TicketControl = require("../models/ticket-control");
 const ticketControl = new TicketControl();
 
 const socketController = (socket) => {
+    
+    socket.emit('ultimo-ticket', ticketControl.ultimo);
 
-    socket.on('enviar-mensaje', ( payload, callback ) => {
+    socket.on('siguiente-ticket', ( payload, callback ) => {
         
-        const id = 123456789;
-        callback( id );
+        const siguiente = ticketControl.siguiente();
+        callback( siguiente );
 
-        socket.broadcast.emit('enviar-mensaje', payload );
+        // TODO: Notificar que hay un nuevo ticket pendiente de asignar
 
     })
 }
