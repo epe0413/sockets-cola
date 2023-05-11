f8616873ec81649b5f2bfe6dd3ab718b45b153f7
Everth Paredes <epe0413@gmail.com>
2023-05-11 17:08:59 -0600

    Tickets pendientes por atender

--- sockets/controller.js
+++ sockets/controller.js
@@ -3,16 +3,18 @@ const TicketControl = require("../models/ticket-control");
 const ticketControl = new TicketControl();
 
 const socketController = (socket) => {
-    
+
+    // Cuando un cliente se conecta
     socket.emit('ultimo-ticket', ticketControl.ultimo);
     socket.emit('estado-actual', ticketControl.ultimos4);
+    socket.emit('tickets-pendientes', ticketControl.tickets.length)
 
     socket.on('siguiente-ticket', ( payload, callback ) => {
         
         const siguiente = ticketControl.siguiente();
         callback( siguiente );
 
-        // TODO: Notificar que hay un nuevo ticket pendiente de asignar
+        socket.broadcast.emit('tickets-pendientes', ticketControl.tickets.length)   
     })
 
     socket.on('atender-ticket', ({escritorio}, callback) => {
@@ -27,6 +29,9 @@ const socketController = (socket) => {
 
         //TODO: Notificar cambio en los ultimo 4
         socket.broadcast.emit('estado-actual', ticketControl.ultimos4);
+        socket.emit('tickets-pendientes', ticketControl.tickets.length)
+        socket.broadcast.emit('tickets-pendientes', ticketControl.tickets.length)
+        
 
         if(!ticket) {
             callback({
